28e7d404b84549400976fd85962447ce0425db65
Ivan <frontenddeveloperweb@gmail.com>
2021-03-06 19:52:15 +0500

    Deleted firebase auth

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-import { AuthProvider } from '../../context/AuthContext'
-
-import { Container } from 'react-bootstrap'
-import './app.scss'
-
-import PrivateRoute from '../private-route/private-route'
-import Signup from '../signup/signup'
-import Dashboard from '../dashboard/dashboard'
-import Login from '../login/login'
+import React from 'react'
 
 export const App = () => {
     return (
-        <>
-            <Container 
-                className="d-flex align-items-center justify-content-center"
-                style={{minHeight: "100vh"}}>
-                <div className="w-100" style={{maxWidth: "400px"}}>
-                    <Router>
-                        <AuthProvider>
-                            <Switch>
-                                <PrivateRoute exact path="/" component={Dashboard} />
-                                <Route path="/signup" component={Signup} />
-                                <Route path="/login" component={Login} />
-                            </Switch>
-                        </AuthProvider>
-                    </Router>
-                </div>
-            </Container>
-        </>
+        <div>
+            
+        </div>
     )
 }
