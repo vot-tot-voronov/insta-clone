24738cbcffb41e5b619f9731d0435d644698a525
Ivan <frontenddeveloperweb@gmail.com>
2021-04-08 12:46:05 +0500

    Refreshing server

--- server/server.js
+++ server/server.js
@@ -1,58 +1,20 @@
 require('dotenv').config()
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const PORT = process.env.PORT || 8000;
 const app = express();
 
-let corsOptions = {
-    origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const db = require("./models/index");
-const Role = db.role;
-
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Db');
-  initial();
-});
+app.use(cors());
+app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.json({message: "Welcome!"});
-});
-
-// routes
-require('./routes/auth.routes')(app);
-require('./routes/user.routes')(app);
-
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-});
-
-
-//delete in production mode and clear sync params
-function initial() {
-    Role.create({
-      id: 1,
-      name: "user"
-    });
-   
-    Role.create({
-      id: 2,
-      name: "moderator"
-    });
-   
-    Role.create({
-      id: 3,
-      name: "admin"
-    });
+const start = async () => {
+  try {
+    app.listen(PORT, (req, res) => console.log(`Server is running on ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+start()
