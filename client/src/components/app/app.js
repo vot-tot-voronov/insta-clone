c503017a69dc3b905b34fb78464563e0bcde1aa6
Ivan <frontenddeveloperweb@gmail.com>
2021-04-10 22:39:13 +0500

    Built current page component

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {HomePage} from '../home-page/HomePage';
 import {Signup} from '../signup/Signup';
 import {Login} from '../login/Login';
+import { CurrentPost } from '../current-post/CurrentPost';
 import 'semantic-ui-css/semantic.min.css';
+import './app.scss';
 
 export const App = () => {
     return (
@@ -13,6 +15,7 @@ export const App = () => {
                     <Route exact path="/" component={HomePage} />
                     <Route path="/signup" component={Signup} />
                     <Route path="/login" component={Login} />
+                    <Route path="/current" component={CurrentPost} />
                 </Switch>
             </Router>
         </>
--- client/src/components/current-post/CurrentPost.js
+++ client/src/components/current-post/CurrentPost.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Image, Button, Segment, Form, TextArea, Icon } from 'semantic-ui-react';
+import ph1 from '../../img/ph1.jpg';
+
 
 export const CurrentPost = () => {
     return (
-        <div>
-            The current post with description, comments and likes
+        <div className="currentPost">
+            <div className="currentPost__block">
+                <Image size="large" src={ph1} />
+                <Segment >
+                    <div className="currentPost__tools">
+                        <Button size="large" icon labelPosition="left" color="red">
+                            <Icon name="like"/>
+                            135 users like this post
+                        </Button>
+                        <Button size="large" icon="tag" color="grey"/>
+                    </div>
+                </Segment>
+                <Segment>
+                    <Form>
+                        <TextArea placeholder='Write comment here...' />
+                        <div className="currentPost__btn currentPost__btn--send">
+                            <Button size="large" color="purple">Send</Button>
+                        </div>
+                    </Form>
+                </Segment>
+            </div>
         </div>
     )
 }
